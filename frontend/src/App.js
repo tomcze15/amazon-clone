b037caf5723d8fb97a9c08effecb05aed944326f
Tomasz Czerwinski <tomasz.czerwinski.1997@gmail.com>
2022-03-19 23:03:49 +0100

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazon</a>
+      </header>
+      <main>listp products</main>
+    </div>
+  );
+}
+
+export default App;
